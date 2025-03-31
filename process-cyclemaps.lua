667ac7d7193ff65e06b4a6d67f26c7121464309d
adrian <adrian-sw@aporter.org>
2025-03-31 18:38:48 -0400

    change some rules about cycleway.

--- process-cyclemaps.lua
+++ process-cyclemaps.lua
@@ -347,7 +347,7 @@ function write_to_transportation_layer(minzoom, highway_class, subclass, ramp, s
 	end
 end
 
-function GetSurface(highway)
+function GetSurface()
 	local surface = Find("surface")
 	local highway = Find("highway")
 	local hiking = Find("hiking")
@@ -404,16 +404,16 @@ function IsCycleway(highway)
 
 	if Find("oneway") == "yes" and Find("oneway:bicycle") == "no" then return true end
 
-	if (highway == "pedestrian" or highway == "living_street" or highway == "path" or highway == "footway" or highway == "steps" or highway == "bridleway" or highway == "corridor" or highway == "track") and (
-		bicycle == "yes" or bicycle == "permissive" or bicycle == "dismount" or bicycle == "customers" or bicycle == "designated" or
-		Holds("ramp:bicycle") and Find("ramp:bicycle") ~= "no" or
-		Find("icn") == "yes" or Holds("icn_ref") or
-		Find("ncn") == "yes" or Holds("ncn_ref") or
-		Find("rcn") == "yes" or Holds("rcn_ref") or
-		Find("lcn") == "yes" or Holds("lcn_ref") or
-		Find("route") == "bicycle"
-	) then
-		return true
+	if GetSurface() == "unpaved" or highway == "pedestrian" or highway == "living_street" or highway == "path" or highway == "footway" or highway == "steps" or highway == "bridleway" or highway == "corridor" or highway == "track" then
+		if bicycle == "yes" or bicycle == "permissive" or bicycle == "dismount" or bicycle == "customers" or bicycle == "designated" or
+			Holds("ramp:bicycle") and Find("ramp:bicycle") ~= "no" or
+			Find("icn") == "yes" or Holds("icn_ref") or
+			Find("ncn") == "yes" or Holds("ncn_ref") or
+			Find("rcn") == "yes" or Holds("rcn_ref") or
+			Find("lcn") == "yes" or Holds("lcn_ref") or
+			Find("route") == "bicycle" then
+			return true
+		end
 	end
 
 	-- todo sport can have multiple values.  match against (;|^)cycling(;|$)
@@ -580,8 +580,8 @@ function way_function()
 		local surface = Find("surface")
 
 		local h = highway
-		if IsCycleway(h) then h = 'cycleway'
-		elseif IsCycleFriendly(h) then h = 'cyclefriendly' end
+		if IsCycleway(h) then h = "cycleway"
+		elseif IsCycleFriendly(h) then h = "cyclefriendly" end
 
 		local is_road = true
 		if h == "" then
