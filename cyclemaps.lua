b4306dce92b819ec50d1db0b3f2561751bdbbc74
adrian <adrian-sw@aporter.org>
2026-06-26 22:06:38 -0400

    highway=service with bicycle=yes is a cycleway?
    it was cyclefriendly before.

--- cyclemaps.lua
+++ cyclemaps.lua
@@ -60,7 +60,7 @@ function IsCycleway(highway)
 	if Find("oneway") == "yes" and Find("oneway:bicycle") == "no" then return true end
 
 	local unpaved = GetSurface() == "unpaved"
-	if unpaved or highway == "pedestrian" or highway == "living_street" or highway == "path" or highway == "footway" or highway == "steps" or highway == "bridleway" or highway == "corridor" or highway == "track" then
+	if unpaved or highway == "pedestrian" or highway == "living_street" or highway == "path" or highway == "footway" or highway == "steps" or highway == "bridleway" or highway == "corridor" or highway == "track" or highway == "service" then
 		-- special logic for sidewalks:  bicycle=yes is enough for CycleFriendly but not Cycleway unless it's unpaved or 3m wide
 		local width = tonumber(Find("width"))
 		if bicycle == "yes" and (unpaved or width ~= nil and width >= 3 or highway ~= "footway" or Find("footway") ~= "sidewalk") then
